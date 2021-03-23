8bfccbe569d0f102ba404dbcf1c5983aa948c14a
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2021-03-23 13:50:15 -0300

    feat: save user email in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { query as q } from "faunadb";
+
+import { fauna } from "@services/fauna";
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,29 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("users_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), {
+              data: {
+                email: user.email,
+              },
+            }),
+            q.Get(q.Match(q.Index("users_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
